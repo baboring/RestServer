981c0e1b57aa19b511aa353cc4df00ec874aff27
ionutpislar <rnpislar@gmail.com>
2014-11-26 14:05:20 +0200

    Added functions for user actions

--- Src/_api/_daemon/UserDaemon/User.cs
+++ Src/_api/_daemon/UserDaemon/User.cs
@@ -11,49 +11,135 @@ namespace RestServer
 {
     public class User : Singleton<User>
     {
+        // Methods
         public User()
         {
-            RegisterAPI();
-        }
-
-        public void RegisterAPI()
-        {
-            #region  Register Version 0
-            api l_api = new api();
-            l_api.version = 0;
-
-            l_api.list.Add(new api_bvh("add", AddData));
-            l_api.list.Add(new api_bvh("get", GetAPI));
-
-            api_container.Instance.apis.Add(l_api);
-            #endregion
-
-            Log.Instance.Write("Registered User API ...");
-        }
-
-       
-
-        public string GetAPI(string rawData)
-        {
-            Dictionary<string, string> tdex = parser.ToDex(rawData);
-
-            string user = tdex["user"];
-
-            return "";
+            this.RegisterAPI();
         }
 
         public string AddData(string rawData)
         {
-            Dictionary<string, string> tdex = parser.ToDex(rawData);
-
-            string user = tdex["user"];
-
+            Dictionary<string, string> dictionary = parser.ToDex(rawData);
+            foreach (string str in dictionary.Keys)
+            {
+                Singleton<Log>.Instance.Write(str);
+            }
+            if (dictionary["format"] == "text")
+            {
+                Singleton<Log>.Instance.Write(dictionary["data"]);
+                this.UserLogText(dictionary["user"], dictionary["data"]);
+            }
+            else
+            {
+                this.SaveImg(dictionary["user"], dictionary["data"]);
+            }
             return "";
         }
 
+        public string BuildDayString()
+        {
+            string str = "";
+            object obj2 = str;
+            return string.Concat(new object[] { obj2, DateTime.UtcNow.Hour, "-", DateTime.UtcNow.Minute, "-", DateTime.UtcNow.Second });
+        }
 
+        public string BuildDayStringS()
+        {
+            string str = "";
+            object obj2 = str;
+            return string.Concat(new object[] { obj2, DateTime.UtcNow.DayOfWeek, "-", DateTime.UtcNow.Month });
+        }
 
+        public void CreateIfNull(string usr)
+        {
+            if (!Directory.Exists("Users/"))
+            {
+                Directory.CreateDirectory("Users");
+            }
+            if (!Directory.Exists("Users/" + usr + "/"))
+            {
+                Directory.CreateDirectory("Users/" + usr + "/");
+            }
+            if (!File.Exists("Users/" + usr + "/usr.cfg"))
+            {
+                StreamWriter writer = new StreamWriter("Users/" + usr + "/usr.cfg");
+                writer.WriteLine("30,1,1,30,50", true);
+                writer.Close();
+            }
+            if (!File.Exists("Users/" + usr + "/" + this.BuildDayStringS()))
+            {
+                new StreamWriter("Users/" + usr + "/" + this.BuildDayStringS() + ".txt", true).Close();
+            }
+            if (!Directory.Exists("Users/" + usr + "/" + this.BuildDayStringS() + "_IMG/"))
+            {
+                Directory.CreateDirectory("Users/" + usr + "/" + this.BuildDayStringS() + "_IMG/");
+            }
+        }
+
+        public string GetAPI(string rawData)
+        {
+            Dictionary<string, string> dictionary = parser.ToDex(rawData);
+            this.CreateIfNull(dictionary["user"]);
+            if (!dictionary.ContainsKey("chmd"))
+            {
+                StreamReader reader = new StreamReader("Users/" + dictionary["user"] + "/usr.cfg");
+                return reader.ReadLine();
+            }
+            return "";
+        }
+
+        public StreamWriter GetUsr(string usr)
+        {
+            if (!Directory.Exists("Users"))
+            {
+                Directory.CreateDirectory("Users");
+            }
+            if (File.Exists("Users/" + usr + ".txt"))
+            {
+                return new StreamWriter("Users/" + usr + ".txt", true);
+            }
+            File.Create("Users/" + usr + ".txt");
+            StreamWriter writer = new StreamWriter("Users/" + usr + ".txt", true);
+            writer.WriteLine("[Created At] = " + DateTime.UtcNow.ToString());
+            return writer;
+        }
+
+        public void RegisterAPI()
+        {
+            api item = new api
+            {
+                version = 0
+            };
+            item.list.Add(new api_bvh("add", new api_callback(this.AddData)));
+            item.list.Add(new api_bvh("g                                                               et", new api_callback(this.GetAPI)));
+            Singleton<api_container>.Instance.apis.Add(item);
+            Singleton<Log>.Instance.Write("Registered User API ...");
+        }
+
+        public void SaveByteArrayAsImage(string fullOutputPath, string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+            File.WriteAllBytes(fullOutputPath, bytes);
+        }
+
+        public void SaveImg(string usr, string img)
+        {
+            this.CreateIfNull(usr);
+            this.SaveByteArrayAsImage("Users/" + usr + "/" + this.BuildDayStringS() + "_IMG/" + this.BuildDayString() + ".png", img);
+        }
+
+        public void UserLogText(string usr, string data)
+        {
+            this.CreateIfNull(usr);
+            StreamWriter writer = new StreamWriter("Users/" + usr + "/" + this.BuildDayStringS() + ".txt", true);
+            writer.WriteLine("[" + this.BuildDayString() + "] = " + data);
+            writer.Close();
+        }
+
+        // Properties
         public StreamWriter sw { get; set; }
     }
+
+
 }
 
